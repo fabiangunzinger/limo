38ebe58fd20d872efc9f28c24261dee464e5e012
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-01-24 11:41:09 +0000

    updated cleaning.

--- helpers.py
+++ helpers.py
@@ -1,34 +1,53 @@
 import os
+import numpy as np
 import pandas as pd
 
-def read_monzo():
+def read_monzo(raw=False):
     """Read data from Monzo google sheet."""
     url = os.environ.get('MONZO_URL')
     sheet_id = os.environ.get('MONZO_SHEETID')
     tab_id = os.environ.get('MONZO_TABID')
-    path = f'{url}{sheet_id}/export?format=csv&gid={tab_id}'
-    df = (pd.read_csv(path, dayfirst=True,
-                      parse_dates={'date': ['Date', 'Time']}))
-    df.columns = (df.columns.str.lower()
-                  .str.replace(' ', '_')
-                  .str.replace('#', ''))
-
-    strs = df.select_dtypes('object')
-    df[strs.columns] = strs.apply(lambda x: x.str.lower())
-    df['amount'] = df.amount.mul(-1)
-    
-    # monthly variable with new month starting on 25th
-    shift = pd.TimedeltaIndex(df.date.dt.daysinmonth - 25 + 1, unit='D')
-    shifted = df.date + shift
-    df['month'] = shifted.dt.to_period('M').dt.to_timestamp()
-    
-    # categorise transfers
-    mask = (
-        (df.category.eq('transfer')) |
-        (df.type.eq('pot transfer')) |
-        (df.notes_and_tags.eq('fgtofg'))
+    fp = f'{url}{sheet_id}/export?format=csv&gid={tab_id}'
+    df = pd.read_csv(fp, dayfirst=True, parse_dates=['Date'])
+    df.columns = (
+        df.columns
+        .str.lower()
+        .str.replace(' ', '_')
+        .str.replace('#', '')
     )
-    df.loc[mask, 'category'] = 'transfer'
+    # lowercase strings and string NA values for easy re search
+    str_cols = df.select_dtypes('object')
+    df[str_cols.columns] = str_cols.apply(lambda x: x.str.lower())
+    df = df.fillna('missing')
+    # short names for lazy typers
+    df = df.rename(columns={
+        'notes_and_tags': 'notes',
+        'category': 'cat',
+        'description': 'desc',
+    })
+    return df
+    
+
+def clean_monzo(df):
+    df = df.copy()
+    useful_cols = ['date', 'desc', 'amount', 'cat', 'name', 'notes', 'type']
+    df = df[useful_cols]
+    
+    # invert amount so spend is positive
+    df['amount'] = df.amount.mul(-1)
+
+    # classify pot ans 'hsbc help to buy' transfers as savings
+    savings = (df.type == 'pot transfer') | (df.desc == 'help to buy')
+    df['cat'] = np.where(savings, 'savings', df.cat)
+    
+    # make income count for next month 
+    offset = pd.offsets.MonthBegin(1)
+    df['date'] = np.where(df.cat == 'income', df.date + offset, df.date) 
+
+    # groups
+    df['group'] = np.where(df.cat == 'income', 'income', None)
+    df['group'] = np.where(df.cat == 'savings', 'savings', df.group)
+    df['group'] = np.where(df.group.isna(), 'spending', df.group)
     return df
 
 
